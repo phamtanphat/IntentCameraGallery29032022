9345e213c43f6907c62e57b06a3db788c53fe887
JoseNguyen <abc@example.com>
2022-06-06 23:33:42 +0700

    change

--- app/src/main/java/com/example/intentcameragallery29032022/MainActivity.java
+++ app/src/main/java/com/example/intentcameragallery29032022/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.intentcameragallery29032022;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewbinding.ViewBinding;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.intentcameragallery29032022.databinding.ActivityMainBinding;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    int CAMERA_PIC_REQUEST = 123;
+    int GALLERY_REQUEST = 456;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +32,50 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},CAMERA_PIC_REQUEST);
+        }
         binding.buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Camera", Toast.LENGTH_SHORT).show();
+                Intent cameraIntent = new Intent();
+                cameraIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
             }
         });
-
         binding.buttonGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Camera", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_PICK);
+                startActivityForResult(intent,GALLERY_REQUEST);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==Activity.RESULT_OK){
+            if(requestCode == CAMERA_PIC_REQUEST) {
+                if (data != null) {
+                    Bitmap image = (Bitmap) data.getExtras().get("data");
+                    binding.imageView.setImageBitmap(image);
+                }
+            }
+            if(requestCode == GALLERY_REQUEST){
+                if (data != null) {
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),data.getData());
+                        binding.imageView.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }else if(resultCode== Activity.RESULT_CANCELED){
+            Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
