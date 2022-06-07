dadcefbd755e1b2f3b18bf553eaaf274464f3463
ogata-higan <lviet456@gmail.com>
2022-06-07 14:49:18 +0700

    Open Cam

--- app/src/main/java/com/example/intentcameragallery29032022/MainActivity.java
+++ app/src/main/java/com/example/intentcameragallery29032022/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.intentcameragallery29032022;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewbinding.ViewBinding;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Toast;
 
@@ -18,10 +26,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        checkPermissionCamera();
 
         binding.buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
                 Toast.makeText(MainActivity.this, "Camera", Toast.LENGTH_SHORT).show();
             }
         });
@@ -32,5 +43,22 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Camera", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void checkPermissionCamera() {
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            },100);
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            binding.imageView.setImageBitmap(captureImage);
+        }
+
     }
 }
